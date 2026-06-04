52f9a6695c20ec0aa019bd756fe5b7c746965c46
Yameen-Shaikh <root@hp-laptop.localdomain>
2026-06-04 16:05:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Mount static folder
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html", context={})
+
+
+@app.get("/patterns", response_class=HTMLResponse)
+async def patterns(request: Request):
+    return templates.TemplateResponse(request=request, name="patterns.html", context={})
+
